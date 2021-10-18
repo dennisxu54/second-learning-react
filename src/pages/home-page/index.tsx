39faae87baf5f9bf61fb60ae318c4d1fe128b0d0
dennis xu <dennisxu54@hotmail.com>
2021-10-19 10:18:16 +1300

    created a type for priority and added form and select components #3

--- src/pages/home-page/index.tsx
+++ src/pages/home-page/index.tsx
@@ -2,12 +2,52 @@ import React, { useState } from "react";
 import "./home-page.css";
 
 function HomePage() {
-  const [title, setTitle] = useState("")
-  const [description, setDescription] = useState("")
-  const [priority, setPriority] = useState("LOW")
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  type priorityType = "LOW" | "MEDIUM" | "HIGH" | string;
+  type Option<T extends priorityType> = {
+    value: T;
+    label: string;
+  };
+  type Props<T extends priorityType> = {
+    options: Option<T>[];
+    value: T;
+    onChange: (value: T) => void;
+  };
+
+  const [priority, setPriority] = useState("LOW");
 
   const handleSubmit = () => {
-    alert(`The title you entered was: ${title} and the description was: ${description} with the priority: ${priority}`)
+    alert(
+      `The title you entered was: ${title} and the description was: ${description} with the priority: ${priority}`
+    );
+  };
+
+  function Select<T extends priorityType>(props: Props<T>) {
+    function handleOnChange(e: React.FormEvent<HTMLSelectElement>) {
+      const { selectedIndex } = e.currentTarget;
+      const selectedOption = props.options[selectedIndex];
+      props.onChange(selectedOption.value);
+    }
+    return (
+      <select value={props.value} onChange={handleOnChange}>
+        {props.options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+    );
+  }
+
+  function Form() {
+    const targets = [
+      { value: "LOW", label: "Low" },
+      { value: "MEDIUM", label: "Medium" },
+      { value: "HIGH", label: "High" },
+    ];
+
+    return <Select options={targets} value={priority} onChange={setPriority} />;
   }
 
   return (
@@ -16,21 +56,25 @@ function HomePage() {
       <form onSubmit={handleSubmit}>
         <label>
           Title:
-          <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
         </label>
         <label>
           Description:
-          <input type="text" value={description} onChange={(e) => setDescription(e.target.value)} />
+          <input
+            type="text"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
         </label>
         <label>
           Priority:
-          <select value={priority} onChange={(e) => setPriority(e.target.value)}>
-            <option value="LOW">LOW</option>
-            <option value="MEDIUM">MEDIUM</option>
-            <option value="HIGH">HIGH</option>
-          </select>
+          <Form />
         </label>
-        <input type="submit"/>
+        <input type="submit" />
       </form>
     </div>
   );
