2cb91d089dd80b0f6e8bc8daab333acbf8fa3127
dennis xu <dennisxu54@hotmail.com>
2021-10-19 13:50:13 +1300

    #3 reverted to old code

--- src/pages/home-page/index.tsx
+++ src/pages/home-page/index.tsx
@@ -5,50 +5,13 @@ function HomePage() {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   type priorityType = "LOW" | "MEDIUM" | "HIGH" | string;
-  type Option<T extends priorityType> = {
-    value: T;
-    label: string;
-  };
-  type Props<T extends priorityType> = {
-    options: Option<T>[];
-    value: T;
-    onChange: (value: T) => void;
-  };
-
-  const [priority, setPriority] = useState("LOW");
+  const [priority, setPriority] = useState<priorityType>("LOW");
 
   const handleSubmit = () => {
     alert(
       `The title you entered was: ${title} and the description was: ${description} with the priority: ${priority}`
     );
   };
-
-  function Select<T extends priorityType>(props: Props<T>) {
-    function handleOnChange(e: React.FormEvent<HTMLSelectElement>) {
-      const { selectedIndex } = e.currentTarget;
-      const selectedOption = props.options[selectedIndex];
-      props.onChange(selectedOption.value);
-    }
-    return (
-      <select value={props.value} onChange={handleOnChange}>
-        {props.options.map((option) => (
-          <option key={option.value} value={option.value}>
-            {option.label}
-          </option>
-        ))}
-      </select>
-    );
-  }
-
-  function Form() {
-    const targets = [
-      { value: "LOW", label: "Low" },
-      { value: "MEDIUM", label: "Medium" },
-      { value: "HIGH", label: "High" },
-    ];
-
-    return <Select options={targets} value={priority} onChange={setPriority} />;
-  }
 
   return (
     <div className="App">
@@ -72,7 +35,11 @@ function HomePage() {
         </label>
         <label>
           Priority:
-          <Form />
+          <select value={priority} onChange={(e) => setPriority(e.target.value)}>
+            <option value="LOW">LOW</option>
+            <option value="MEDIUM">MEDIUM</option>
+            <option value="HIGH">HIGH</option>
+          </select>
         </label>
         <input type="submit" />
       </form>
